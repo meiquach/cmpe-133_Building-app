eb8d2fc568d50c869bb0585dc07ad84a77d52ca4
DatTriTat <dattri.tat@sjsu.edu>
2022-03-15 13:02:27 -0700

    add blog.js and service.js

--- src/Components/Pages/Pages.js
+++ src/Components/Pages/Pages.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import Video from "./Video";
+import video1 from "./images/vid-1.mp4";
 import video2 from "./images/vid-2.mp4";
 import video3 from "./images/vid-3.mp4";
+import video4 from "./images/vid-4.mp4";
 import video5 from "./images/vid-5.mp4";
 
 function Pages() {
-  const [video, setVideo] = useState(video2);
-  const [actives, setActives] = useState(["active", null, null]);
+  const [video, setVideo] = useState(video1);
+  const [actives, setActives] = useState(["active", null, null, null, null]);
 
-  const videos = [video2, video3, video5];
+  const videos = [video1, video2, video3, video4, video5];
 
   const handleButtonClick = (index) => {
-    const newActives = [null, null, null];
+    const newActives = [null, null, null, null, null];
     newActives[index] = "active";
     setActives(newActives);
     setVideo(videos[index]);
@@ -28,7 +30,7 @@ function Pages() {
       </div>
 
       <div className="controls">
-        {[0, 1, 2].map((index) => {
+        {[0, 1, 2, 3, 4].map((index) => {
           return (
             <button
               key={index}
